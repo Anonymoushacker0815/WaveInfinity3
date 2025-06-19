86535a09f88550b2748ea8741ad489bc94e84842
Lukas Eberhart <166514343+Anonymoushacker0815@users.noreply.github.com>
2025-06-19 15:11:49 +0200

    FireballsScale

--- Scripts/fireball.gd
+++ Scripts/fireball.gd
@@ -1,12 +1,22 @@
 extends Area2D
 
-@export var speed   := 200.0
-@export var damage  := 20
+@export var speed   := 200.0 
+@export var damage  := 20    
 
 var velocity := Vector2.ZERO
 
 func _ready():
+	var lvl := 1
+	var root = get_tree().current_scene
+	if root.has_node("LevelManager"):
+		lvl = root.get_node("LevelManager").level
+
+	var stat_mult = pow(1.1, lvl - 1)
+	speed  *= stat_mult
+	damage = int(damage * stat_mult)
+
 	velocity = Vector2.RIGHT.rotated(rotation) * speed
+
 	connect("body_entered", Callable(self, "_on_body_entered"))
 
 func _physics_process(delta):
@@ -18,5 +28,6 @@ func _on_body_entered(body):
 			body.take_damage(damage)
 		queue_free()
 		return
+
 	if body.is_in_group("world_boundary"):
 		queue_free()
